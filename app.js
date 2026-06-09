83f523030af7bc7c4a08cc3de86ad2f7594038fb
Fabio Dias <kinzaum@gmail.com>
2026-06-08 18:54:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let currentLang = {};
 let isEditMode = false;
 let pendingAction = null;
 let currentPage = 0; // Keep it here!
+let selectedSwapId = null;
+// Selection states
+let selectedSwapCardId = null;
+let selectedSwapBoardId = null;
 
 let appState = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
     activeBoardId: null,
